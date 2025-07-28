6b3e1fb1949f913402eec42feba148b0e3e67cce
Alok Kumar <16762323+aloklearning@users.noreply.github.com>
2025-07-28 20:02:59 +0530

    Delete images directory

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+# Namastey React Learning
+
+A project created to learn in-depth of React fundamentals while creating a production ready app from the course [Namastey React](https://namastedev.com/learn/namaste-react) 🚀, created by Akshay Saini.
+
 <!-- LICENSE -->
+
 ## License
 
 Distributed under the MIT License. See `LICENSE.txt` for more information.
--- react-cdn/App.js
+++ react-cdn/App.js
@@ -10,29 +10,17 @@
  *      </div>
  * </div>
  */
-const parent = React.createElement(
-    "div", 
-    { id: "parent" },
-    [
-        React.createElement(
-            "div", 
-            { id: "child" }, 
-            [
-                React.createElement("h1", {}, "I am h1 tag"),
-                React.createElement("h2", {}, "I am h2 tag")
-            ]
-        ),
-        React.createElement(
-            "div", 
-            { id: "child2" }, 
-            [
-                React.createElement("h1", {}, "I am h1 tag"),
-                React.createElement("h2", {}, "I am h2 tag")
-            ]
-        ),
-    ]
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+console.log(parent); // Just to see that parent is an object and not an element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent); // This converts ReactElement(parent) -> HTML DOM Element
