1ebc407a6e1adac44e715051555e4a6588303792
aloklearning <16762323+aloklearning@users.noreply.github.com>
2025-08-06 18:15:12 +0530

    Add useState case to filtering restaurants

--- react-app-package/src/components/Body.js
+++ react-app-package/src/components/Body.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 import restaurants from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [resLists, setResLists] = useState(restaurants);
+
   return (
     <div style={{ padding: "10px" }}>
-      <p>Search Bar</p>
+      <div className="filter-button">
+        <button
+          onClick={() =>
+            setResLists((res) =>
+              res.filter((item) => item.info.avgRating >= 4.5)
+            )
+          }>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {restaurants.map((restaurant) => (
+        {resLists.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
